eddac3cdbeffd3045ff64a09918df818e0b3a65a
yashpalsinh6 <75825134+yashpalsinh6@users.noreply.github.com>
2024-05-01 15:32:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+ document.addEventListener('DOMContentLoaded', function() {
+    const body = document.querySelector('body');
+    body.textContent = 'DOM load success';
+  });
